976dfc7dd91ac4a87b00d087bf9b3ea4f29fd177
Zehan Khan <zkhan@hootboard.com>
2023-11-09 00:36:44 +0530

    build(init project with spring): :tada: initial commit

---
+++ instagram-api/src/main/java/com/example/instagramapi/InstagramApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.instagramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramApiApplication.class, args);
+	}
+
+}
---
+++ instagram-api/src/test/java/com/example/instagramapi/InstagramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instagramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
